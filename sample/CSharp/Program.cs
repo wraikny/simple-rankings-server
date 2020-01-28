83593164d7fa7e74f03e7afb510a80a8ff9753d2
wraikny <writtenknown@gmail.com>
2020-01-28 19:27:32 +0900

    Add: C# Sample

---
+++ sample/CSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using SimpleRankingsServer;
+
+namespace CSharp
+{
+    class Program
+    {
+        [DataContract]
+        class Sample1
+        {
+            [DataMember]
+            public int Score1 { get; set; }
+            [DataMember]
+            public double Score2 { get; set; }
+            [DataMember]
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return $"{{ \"Score1\" : {Score1}, \"Score2\" : {Score2}, \"Name\" : {Name} }}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            const string Url = @"http://localhost:8080/v1/Sample1";
+
+            var userId = Guid.NewGuid();
+
+            var sample = new Sample1 { Score1 = 118, Score2 = 204.6, Name = "kitsune" };
+            var result = Client.Insert(Url, userId.ToString(), sample).Result;
+
+            Console.WriteLine(result);
+
+            var data = Client.Select<Sample1>(Url, orderBy: "Id", limit: 5).Result;
+            foreach (var x in data)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sample/CSharp/SimpleRankingsServer.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace SimpleRankingsServer
+{
+    [DataContract]
+    public class Data<T>
+    {
+        [DataMember(Name = "id")]
+        public long Id { get; set; }
+        [DataMember(Name = "userId")]
+        public string UserId { get; set; }
+        [DataMember(Name = "values")]
+        public T Values { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{ \"id\" : {Id}, \"userId\" : {UserId}, \"values\" : {Values.ToString()} }}";
+        }
+    }
+
+    public static class JsonUtils
+    {
+        public static string Serialize<T>(T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T));
+                serializer.WriteObject(ms, obj);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T));
+                return (T)serializer.ReadObject(ms);
+            }
+        }
+    }
+
+    public static class Client
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        [DataContract]
+        private class InsertParam<T>
+        {
+            [DataMember(Name = "userId")]
+            public string UserId { get; set; }
+            [DataMember(Name = "values")]
+            public T Values { get; set; }
+        }
+
+        [DataContract]
+        private class InsertResult
+        {
+            [DataMember(Name = "id")]
+            public long Id { get; set; }
+        }
+
+        public static async Task<long> Insert<T>(string url, string userId, T data)
+        {
+            var json = JsonUtils.Serialize(new InsertParam<T> { UserId = userId, Values = data });
+            using (var content = new StringContent(json, Encoding.UTF8, @"application/json"))
+            {
+                var result = await client.PostAsync(url, content);
+                var resString = await result.Content.ReadAsStringAsync();
+
+                if (result.IsSuccessStatusCode)
+                {
+                    return JsonUtils.Deserialize<InsertResult>(resString).Id;
+                }
+
+                throw new Exception($"{result.StatusCode}:{resString}");
+            }
+        }
+
+        public static async Task<Data<T>[]> Select<T>(string url, string orderBy = null, bool? isDescending = null, int? limit = null)
+        {
+            var param = new Dictionary<string, string>();
+
+            if (orderBy != null) param.Add("orderBy", orderBy);
+            if (isDescending != null) param.Add("isDescending", isDescending.Value.ToString());
+            if (limit != null) param.Add("limit", limit.Value.ToString());
+
+            var result = await client.GetAsync($"{url}?{await new FormUrlEncodedContent(param).ReadAsStringAsync()}");
+            var resString = await result.Content.ReadAsStringAsync();
+
+            if (result.IsSuccessStatusCode)
+            {
+                return JsonUtils.Deserialize<Data<T>[]>(resString);
+            }
+
+            throw new Exception($"{result.StatusCode}:{resString}");
+        }
+    }
+}
